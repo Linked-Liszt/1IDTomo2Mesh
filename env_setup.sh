6dfb9837abf236069aa85b4611f4b8931095e008
Linked-Liszt <anoxymoren@gmail.com>
2023-02-21 13:11:56 -0600

    Add additional environment setup

--- env_setup.sh
+++ env_setup.sh
@@ -1,4 +1,7 @@
 # Assumes fresh conda env python=3.9
 conda install -y -c conda-forge cudatoolkit=11.2 cudnn=8.1.0
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
+conda install -y -c astra-toolbox astra-toolbox
+conda install -y -c astra-toolbox astra-toolbox
+conda install -y -c conda-forge tomopy
 pip install -r requirements_pip.txt
